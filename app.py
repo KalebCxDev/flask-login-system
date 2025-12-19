0a73fb6f754e29a9ed1583bd1e3070083c63490a
Kaleb <examplegpt404@gmail.com>
2025-12-19 16:08:44 -0500

    Coreccion

--- app.py
+++ app.py
@@ -296,6 +296,5 @@ def archivo_demasiado_grande(e):
 
 with app.app_context(): db.create_all()
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0",
- port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
