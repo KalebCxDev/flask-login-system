927b997e11deeff06d2a79dcf5a1f8af9ba46dc6
Caleb <158231297+KalebCxDev@users.noreply.github.com>
2025-12-19 10:37:56 -0500

    Cambio de puerto y desactivar modo debug

--- app.py
+++ app.py
@@ -283,4 +283,7 @@ def archivo_demasiado_grande(e):
 
 with app.app_context(): db.create_all()
 
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",
+ port=port)
